a0e81fa0a6be1ed597c8596dc8f64b42c35818c0
Henry Kelder <hhkelder@gmail.com>
2023-06-08 01:39:00 +0200

    clean

--- Superlink/api/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from Superlink.api.access_token_api import AccessTokenApi
-from Superlink.api.health_api import HealthApi
-from Superlink.api.resolution_api import ResolutionApi
-
--- SuperlinkAPI/api/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from SuperlinkAPI.api.access_token_api import AccessTokenApi
-from SuperlinkAPI.api.health_api import HealthApi
-from SuperlinkAPI.api.resolution_api import ResolutionApi
-
